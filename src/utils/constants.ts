6d70293aab705575be510d773593876cc73b4581
devjamb <devjamber@gmail.com>
2025-10-17 08:25:14 +0100

    added new user for testing

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -531,4 +531,5 @@ export const ADMIN_WALLETS = [
   "0x1d3420a527c4002338b20f22e5dcd48fb44a4e51",
   "0x3f9b81397ab61e18092291287d05f44d24fd53c6",
   "0xe4cdf1ddc97bd5dee8a0f0044e1260856e738d49",
+  "0xde336ed12ea3b8feb4aeaa07b717588fc79a8181",
 ];
